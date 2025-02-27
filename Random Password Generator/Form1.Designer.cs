7f328a225ba62d935246b15f01b2d4c611b3f670
jegerikkeblizter <mathiashansen2007@gmail.com>
2025-02-27 12:33:57 +0100

    Added button and label. also added a random function for the password to be generated. when generated label gets changed to the random password.

--- Random Password Generator/Form1.Designer.cs
+++ Random Password Generator/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate your password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(308, 182);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(181, 20);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password will come here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPassword;
     }
 }
 
--- Random Password Generator/Form1.cs
+++ Random Password Generator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Random_Password_Generator
 {
@@ -15,6 +16,17 @@ namespace Random_Password_Generator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            int length = 10;
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?";
+            var random = new Random();
+            string password = new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            labelPassword.Text = $"Generated Password: {password}";
         }
     }
 }
